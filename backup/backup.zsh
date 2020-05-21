a64ed3bf2ed9838ca036d80cd5e8c7413517716d
Anton Vasin <antonvasin@me.com>
2020-05-21 21:52:29 +0300

    updating dotfiles on Thu May 21 18:52:29 UTC 2020

--- backup/backup.zsh
+++ backup/backup.zsh
@@ -3,6 +3,8 @@ DOTFILE_DIR="/Users/antonvasin/dotfiles"
 
 cd "$DOTFILE_DIR" || exit
 
+brew bundle dump --force --global
+
 if ! git diff --quiet HEAD || git status --short; then
   git add --all
   git commit -m "updating dotfiles on $(date -u)"
