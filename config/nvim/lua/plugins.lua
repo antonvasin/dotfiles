329317bbd234671fc0a5a6701e18b28c96d5933e
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2023-06-13 11:32:11 +0100

    updating dotfiles on Tue Jun 13 10:32:11 UTC 2023

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -164,6 +164,7 @@ return packer.startup(function(use)
 			"hrsh7th/cmp-nvim-lsp",
 			"saadparwaiz1/cmp_luasnip",
 			"L3MON4D3/LuaSnip",
+			"rafamadriz/friendly-snippets",
 			"hrsh7th/cmp-emoji",
 			"onsails/lspkind.nvim",
 			"b0o/schemastore.nvim",
