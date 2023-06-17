8a92a87cd71a0f1cc69246d213bf6a0c28ae8b4b
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2023-06-17 15:33:33 +0100

    updating dotfiles on Sat Jun 17 14:33:33 UTC 2023

--- config/nvim/lua/ui.lua
+++ config/nvim/lua/ui.lua
@@ -40,12 +40,29 @@ vim.cmd([[
 
 vim.cmd("autocmd VimResized * wincmd =")
 
+vim.opt.winblend = 25
+vim.opt.pumblend = 25
+
 if vim.g.neovide then
-  -- vim.g.neovide_floating_blur_amount_x = 8.0
-  -- vim.g.neovide_floating_blur_amount_y = 8.0
-  vim.g.neovide_cursor_trail_size = 0.01
-  vim.g.neovide_hide_mouse_when_typing = true
-  vim.o.guifont = "JetBrainsMono Nerd Font Mono:h14"
+	vim.g.neovide_cursor_trail_size = 0.05
+	vim.g.neovide_cursor_animation_length = 0.07
+	vim.g.neovide_hide_mouse_when_typing = true
+	vim.o.guifont = "JetBrainsMono Nerd Font Mono:h14"
+	vim.g.neovide_remember_window_size = true
+
+	-- Allow clipboard copy paste in neovim
+	vim.g.neovide_input_use_logo = 1
+	vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+	vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+	vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+	vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+	vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+	vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+
+	vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 end
 
 vim.opt.signcolumn = "yes"
