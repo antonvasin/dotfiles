e8072257aaa07f4b5a74bb46a8417c3d1f60a754
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2023-11-27 20:34:37 +0000

    Updating dotfiles on Mon Nov 27 20:34:37 UTC 2023

--- backup/backup.zsh
+++ backup/backup.zsh
@@ -6,10 +6,10 @@ cd "$DOTFILE_DIR" || exit
 brew bundle dump --force --global
 
 # Backup iTerm config
-cp /Users/antonvasin/Library/Mobile Documents/com~apple~CloudDocs/com.googlecode.iterm2.plist iTerm/
+cp "/Users/antonvasin/Library/Mobile Documents/com~apple~CloudDocs/com.googlecode.iterm2.plist" iTerm/
 
 if ! git diff --quiet HEAD || git status --short; then
   git add --all
-  git commit -m "updating dotfiles on $(date -u)"
+  git commit -m "Updating dotfiles on $(date -u)"
   git push origin main
 fi
