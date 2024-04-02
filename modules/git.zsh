3dc4f8bf5ba6a697a59e9e0a7cc8ad9ef987e427
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2024-04-03 00:19:46 +0100

    add ghR

--- modules/git.zsh
+++ modules/git.zsh
@@ -6,4 +6,9 @@ alias gpft='git push --follow-tags'
 alias gpd='git push --delete'
 alias gsd='git diff --staged'
 alias gpf='git push --force-with-lease'
-alias ghL="gh repo view -w"
+alias ghv="gh repo view -w"
+
+ghR() {
+  remote_url="$(gh repo view $1 --json url --jq .url).git"
+  git remote add origin $remote_url
+}
