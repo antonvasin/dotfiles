8fb9a6c0a3167957583182ba5a2f61becde64177
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2024-06-01 22:05:28 +0100

    Updating dotfiles on Sat Jun  1 21:05:28 UTC 2024

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -5,4 +5,67 @@ config.font = wezterm.font("Iosevka Term")
 config.font_size = 15.0
 config.color_scheme = "Papercolor Dark (Gogh)"
 
+config.native_macos_fullscreen_mode = true
+
+config.keys = {
+  {
+    key = "h",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.ActivatePaneDirection("Left"),
+  },
+  {
+    key = "l",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.ActivatePaneDirection("Right"),
+  },
+  {
+    key = "k",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.ActivatePaneDirection("Up"),
+  },
+  {
+    key = "j",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.ActivatePaneDirection("Down"),
+  },
+  {
+    key = "h",
+    mods = "CTRL|SHIFT|ALT",
+    action = wezterm.action.SplitPane({
+      direction = "Left",
+    }),
+  },
+  {
+    key = "l",
+    mods = "CTRL|SHIFT|ALT",
+    action = wezterm.action.SplitPane({
+      direction = "Right",
+    }),
+  },
+  {
+    key = "k",
+    mods = "CTRL|SHIFT|ALT",
+    action = wezterm.action.SplitPane({
+      direction = "Up",
+    }),
+  },
+  {
+    key = "j",
+    mods = "CTRL|SHIFT|ALT",
+    action = wezterm.action.SplitPane({
+      direction = "Down",
+    }),
+  },
+  {
+    key = "w",
+    mods = "CMD",
+    action = wezterm.action.CloseCurrentPane({ confirm = true }),
+  },
+  {
+    key = "f",
+    mods = "CTRL|CMD",
+    action = wezterm.action.ToggleFullScreen,
+  },
+}
+
 return config
