63afa499ea7c58fac01e125c7a4c2f93e834d857
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2024-06-19 21:06:15 +0100

    Updating dotfiles on Wed Jun 19 20:06:15 UTC 2024

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -74,7 +74,12 @@ require("lazy").setup({
   { "ibhagwan/fzf-lua" },
 
   -- Integrations
-  "mileszs/ack.vim",
+  {
+    "mileszs/ack.vim",
+    init = function()
+      vim.g.ackprg = 'rg --sort path --vimgrep --smart-case --no-heading'
+    end,
+  },
   {
     "tpope/vim-fugitive",
     dependencies = {
@@ -478,11 +483,11 @@ if vim.g.neovide then
 
   -- Allow clipboard copy paste in neovim
   vim.g.neovide_input_use_logo = 1
-  vim.keymap.set("n", "<D-s>", ":w<CR>")     -- Save
-  vim.keymap.set("v", "<D-c>", '"+y')        -- Copy
-  vim.keymap.set("n", "<D-v>", '"+P')        -- Paste normal mode
-  vim.keymap.set("v", "<D-v>", '"+P')        -- Paste visual mode
-  vim.keymap.set("c", "<D-v>", "<C-R>+")     -- Paste command mode
+  vim.keymap.set("n", "<D-s>", ":w<CR>")      -- Save
+  vim.keymap.set("v", "<D-c>", '"+y')         -- Copy
+  vim.keymap.set("n", "<D-v>", '"+P')         -- Paste normal mode
+  vim.keymap.set("v", "<D-v>", '"+P')         -- Paste visual mode
+  vim.keymap.set("c", "<D-v>", "<C-R>+")      -- Paste command mode
   vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
 
   vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
