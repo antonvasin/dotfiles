1e26d316806d619a93211820279b391373fd578b
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2024-07-20 14:48:11 +0100

    Updating dotfiles on Sat Jul 20 13:48:11 UTC 2024

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -323,9 +323,6 @@ local on_attach = function(client, bufnr)
     end
   end
 
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
