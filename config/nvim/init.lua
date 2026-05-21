998ca4191702ef990ce2f335897184523cab966b
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2026-05-21 20:22:57 +0100

    Updating dotfiles on Thu May 21 19:22:57 UTC 2026

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1071,21 +1071,21 @@ require('Comment').setup {
 
 null_ls.setup({
   sources = {
-    -- null_ls.builtins.formatting.prettier.with({
-    --   prefer_local = "node_modules/.bin",
-    --   filetypes = {
-    --     "javascript",
-    --     "javascriptreact",
-    --     "typescript",
-    --     "typescriptreact",
-    --     "css",
-    --     "scss",
-    --     "html",
-    --     "json",
-    --     "yaml",
-    --     "graphql",
-    --   },
-    -- }),
+    null_ls.builtins.formatting.prettier.with({
+      prefer_local = "node_modules/.bin",
+      filetypes = {
+        "javascript",
+        "javascriptreact",
+        "typescript",
+        "typescriptreact",
+        "css",
+        "scss",
+        "html",
+        "json",
+        "yaml",
+        "graphql",
+      },
+    }),
     -- null_ls.builtins.diagnostics.actionlint,
     -- null_ls.builtins.diagnostics.eslint,
     -- null_ls.builtins.code_actions.eslint,
