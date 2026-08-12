fb14b875e9123d10b3162f4ffcd840225890df5d
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2026-08-12 22:27:16 +0100

    Updating dotfiles on Wed Aug 12 21:27:16 UTC 2026

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -732,12 +732,12 @@ cmp.setup({
     { name = "lazydev" },
     { name = "emoji" },
   },
-  completion = {
-    winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
-    col_offset = -3,
-    side_padding = 0,
-    autocomplete = false,
-  },
+  -- completion = {
+  --   winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+  --   col_offset = -3,
+  --   side_padding = 0,
+  --   autocomplete = false,
+  -- },
   window = {
     completion = cmp.config.window.bordered({}),
     documentation = cmp.config.window.bordered({}),
